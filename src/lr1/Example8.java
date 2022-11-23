1225d0cbc435eca059141edf16523e3d4a206d66
Nekrasoov <derill-mist@yandex.ru>
2022-11-24 00:11:44 +0500

    3

---
+++ src/lr1/Example8.java
@@ -0,0 +1,20 @@
+package lr1;
+
+import java.util.Scanner;
+
+public class Example8 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Input day: ");
+        int day = in.nextInt();
+
+        System.out.println("Input month: ");
+        int month = in.nextInt();
+
+        System.out.println("Input year: ");
+        int year = in.nextInt();
+
+        System.out.printf("Day: %d  Month: %d  Year: %d", day, month, year);
+    }
+}
