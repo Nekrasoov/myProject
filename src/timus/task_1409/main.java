02a5c64f2bcf8c726654ffc2fefe80340ac4cc5e
Nekrasoov <derill-mist@yandex.ru>
2022-12-04 16:38:47 +0500

    timus 1409

--- src/timus/task_1409/main.java
+++ src/timus/task_1409/main.java
@@ -1,4 +1,22 @@
 package timus.task_1409;
 
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        int G = in.nextInt();;
+        int L = in.nextInt();;
+
+
+        int resultG = (G + L - 1) - G;
+        int resultL = (G + L - 1) - L;
+
+        out.print(resultG + " " + resultL);
+        out.flush();
+    }
 }
+
